9507a561ab3e568474102c467a4282303cb8f7a1
Fryann <fryann14@hotmail.com>
2018-06-03 12:33:49 -0400

    Adding Profile Activity

--- app/src/main/java/com/example/fryan/practica_login_01/MainActivity.java
+++ app/src/main/java/com/example/fryan/practica_login_01/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.fryan.practica_login_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+     private Button ButtonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         ButtonLogin =  findViewById(R.id.LoginButon);
+
+
+         ButtonLogin.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                 startActivity(intent);
+
+
+             }
+         });
+
+
+       // Intent intent = new
     }
 }
