f904b47c77afd9e060703f6e758defebdd93cb3a
Fryann <fryann14@hotmail.com>
2018-06-05 23:51:05 -0400

    ShareButton Ready

--- app/src/main/java/com/example/fryan/practica_login_01/MainActivity.java
+++ app/src/main/java/com/example/fryan/practica_login_01/MainActivity.java
@@ -12,59 +12,86 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-     private Button ButtonLogin;
-     private EditText Edit_User, Edit_Psw;
-     private TextView Tv_mensaje;
-     private int counter = 3;
+    private Button ButtonLogin;
+    private EditText Edit_User, Edit_Psw;
+    private TextView Tv_mensaje;
+    private int counter = 3;
+    private Button ButtonShare;
+    private  String Mensaje ="HOla Soy Fryann un Software Developer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         ButtonLogin =  findViewById(R.id.LoginButon);
-         Edit_User = findViewById((R.id.Email_id));
-         Edit_Psw = findViewById(R.id.password_id);
-         Tv_mensaje = findViewById(R.id.mensaje_id);
+
+        ButtonLogin = findViewById(R.id.LoginButon);
+        Edit_User = findViewById((R.id.Email_id));
+        Edit_Psw = findViewById(R.id.password_id);
+        Tv_mensaje = findViewById(R.id.mensaje_id);
+        ButtonShare = findViewById(R.id.shareProfile_id);
+
+        findViewById(R.id.LoginButon).setOnClickListener(this);
+        findViewById(R.id.Button_shareProfile).setOnClickListener(this);
 
 
-         ButtonLogin.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
 
-
-
-                  if (Edit_User.getText().toString().equals("fryannm@gmail.com") && Edit_Psw.getText().toString().equals("*"))
-
-                  {
-                      Toast.makeText(getApplicationContext(),
-                              "Redirecting...",Toast.LENGTH_SHORT).show();
-                      Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
-                      intent.putExtra("name",Edit_User.getText().toString());
-                      startActivity(intent);
-                  }
-                  else {
-
-
-                     Toast.makeText(getApplicationContext(), "Wrong Password or email address",Toast.LENGTH_SHORT).show();
-
-                     Edit_User.setVisibility(View.VISIBLE);
-                     Tv_mensaje.setBackgroundColor(Color.RED);
-                     counter --;
-                     Tv_mensaje.setText(Integer.toString(counter));
-
-
-                      if (counter == 0)
-                      {
-                          Toast.makeText(getApplicationContext(),"Try again Later ",Toast.LENGTH_SHORT).show();
-                          ButtonLogin.setEnabled(false);
-                          finish();
-                      }
-                 }
-             }
-         });
     }
-}
+        @Override
+        public void onClick (View v){
+
+            switch (v.getId()) {
+                case R.id.LoginButon: {
+
+                    if (Edit_User.getText().toString().equals("fryannm@gmail.com") && Edit_Psw.getText().toString().equals("*"))
+
+                    {
+                        Toast.makeText(getApplicationContext(),
+                                "Redirecting...", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                        intent.putExtra("name", Edit_User.getText().toString());
+                        startActivity(intent);
+                    } else {
+
+
+                        Toast.makeText(getApplicationContext(), "Wrong Password or email address", Toast.LENGTH_SHORT).show();
+
+                        Edit_User.setVisibility(View.VISIBLE);
+                        Tv_mensaje.setBackgroundColor(Color.RED);
+                        counter--;
+                        Tv_mensaje.setText(Integer.toString(counter));
+
+
+                        if (counter == 0) {
+                            Toast.makeText(getApplicationContext(), "Try again Later ", Toast.LENGTH_SHORT).show();
+                            ButtonLogin.setEnabled(false);
+                            finish();
+                        }
+                    }
+
+                }
+
+                case R.id.Button_shareProfile: {
+
+
+                        Intent intent  = new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_SUBJECT,Mensaje);
+                        intent.putExtra(Intent.EXTRA_TEXT,Mensaje);
+                        startActivity(Intent.createChooser(intent,"Share this "));
+
+
+                }
+
+            }
+
+
+        }
+    }
+
+
+
+
